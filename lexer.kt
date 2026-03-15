97439254404a3cc554be71e84ad1b4d38c8a87d3
_111y <huy264128@gmail.com>
2026-03-15 17:11:07 +0800

    making lexer

---
+++ lexer.kt
@@ -0,0 +1,52 @@
+val specialCharacterTable = hashSetOf(
+    '+',
+    '-',
+    '*',
+    '/',
+    '=',
+    '(',
+    ')',
+    ',',
+    ';',
+    ':',
+    '&',
+    '^',
+    '%',
+    '$',
+    '#',
+    '@',
+    '!',
+    '<',
+    '>',
+    '?',
+    '\"',
+    '\'',
+    '.',
+    '[',
+    ']',
+    '{',
+    '}',
+    '\\'
+)
+val ignoredCharacterTable = hashSetOf(' ', '\n', '\t')
+fun splitString(rawString:String): ArrayList<String> {
+    val splitStr = arrayListOf<String>()
+    var strBuffer=""
+    for(character in rawString){
+        if(specialCharacterTable.contains(character)){
+            if(strBuffer!=""){
+                splitStr.add(strBuffer)
+                strBuffer=""
+            }
+            splitStr.add(character.toString())
+        }else if(ignoredCharacterTable.contains(character)){
+            if(strBuffer!=""){
+                splitStr.add(strBuffer)
+                strBuffer=""
+            }
+        }else {
+            strBuffer+=character
+        }
+    }
+    return splitStr
+}
