e6beee597cb265b266850538e8928a55f52fd65c
_111y <huy264128@gmail.com>
2026-03-17 20:38:01 +0800

    rebuild lexer

--- lexer.kt
+++ lexer.kt
@@ -25,14 +25,15 @@ val operators = setOf(
     '|'
 )
 
-data class token(val type: Int, val value: String) //define same as state
+data class Token(val type: Int, val value: String) //define same as state
 
 suspend fun lexer(rawText: String) {
     var state: Int = 0  // 0 -> Null Statement 1-> Identify 2->Int 3->Float 4:Char 5:String 6:Operator
     var catchSlash: Boolean = false //  捕捉到了反斜杠时为 true
     var lexerBuffer: String = ""
-    var tokenList = mutableListOf<token>()
-    for (character in rawText) {
+    var tokenList = mutableListOf<Token>()
+    for (character: Char in rawText) {
+
         when (state) {
             0 -> {
                 when {
@@ -57,7 +58,7 @@ suspend fun lexer(rawText: String) {
 
             1 -> {
                 if (operators.contains(character) || setOf('\\', '"', '\'', ' ', '\n', '\t').contains(character)) {
-                    tokenList.add(token(1, lexerBuffer))
+                    tokenList.add(Token(1, lexerBuffer))
                     lexerBuffer = ""
                     if (operators.contains(character)) {
                         lexerBuffer += character
@@ -75,7 +76,7 @@ suspend fun lexer(rawText: String) {
                     continue
                 }
                 if (operators.contains(character) || setOf('\\', '"', '\'', ' ', '\n', '\t').contains(character)) {
-                    tokenList.add(token(1, lexerBuffer))
+                    tokenList.add(Token(1, lexerBuffer))
                     lexerBuffer = ""
                     if (operators.contains(character)) {
                         lexerBuffer += character
@@ -85,10 +86,70 @@ suspend fun lexer(rawText: String) {
                     state = 0
                     continue
                 }
-                if (character.isDigit())
-                    lexerBuffer += character
-                else
-                    throw IllegalStateException("string $lexerBuffer is not a illegal value")
+                if (character.isDigit()) lexerBuffer += character
+                else throw IllegalStateException("string $lexerBuffer is not a illegal value")
+            }
+
+            3 -> {
+                if (operators.contains(character) || setOf('\\', '"', '\'', ' ', '\n', '\t').contains(character)) {
+                    tokenList.add(Token(1, lexerBuffer))
+                    lexerBuffer = ""
+                    if (operators.contains(character)) {
+                        lexerBuffer += character
+                        state = 6
+                        continue
+                    }
+                    state = 0
+                    continue
+                }
+                if (character.isDigit()) lexerBuffer += character
+                else throw IllegalStateException("string $lexerBuffer is not a illegal value")
+            }
+
+            4 -> {
+                if (character == '\\' && !catchSlash) {
+                    catchSlash = true
+                    continue
+                }
+                val currentCharacter: Char = when (catchSlash) {
+                    true -> run {
+                        val mappedChar = mapOf('n' to '\n', 't' to '\t', '\'' to '\'')[character]
+                        if (mappedChar != null) return@run mappedChar
+                        return@run character
+                    }
+
+                    false -> character
+                }
+                catchSlash = false
+                if (character == '\'') {
+                    tokenList.add(Token(4, lexerBuffer))
+                    lexerBuffer = ""
+                    continue
+                }
+                if (lexerBuffer != "") {
+                    throw IllegalStateException("Type Char must have only one Character \u270B\uD83D\uDE2D\uD83E\uDD1A")
+                } else {
+                    lexerBuffer += currentCharacter
+                }
+            }
+
+            5 -> {
+                if (character == '\\' && !catchSlash) {
+                    catchSlash = true
+                    continue
+                }
+                if (catchSlash) {
+                    val currentChar = mapOf('n' to '\n', 't' to '\t', '\'' to '\'')[character];
+                    if (currentChar != null) lexerBuffer += currentChar
+                    else lexerBuffer += character
+                    continue
+                }
+                if (character == '"') {
+                    tokenList.add(Token(4, lexerBuffer))
+                    lexerBuffer = ""
+                    continue
+                }
+                lexerBuffer += character
             }
         }
     }
