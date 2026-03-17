2107637227d1c45f005f8ef3d3abc78a060cff5e
_111y <huy264128@gmail.com>
2026-03-17 12:11:24 +0800

    making lexer

--- ast.kt
+++
@@ -1,5 +0,0 @@
-
-class astInterpreter{
-    private var varibleNameMapping= mutableMapOf<String,Int>()
-    private var className = mutableMapOf<String,Int>("int" to 1, "float" to 2,"bool" to 3)
-}
--- lexer.kt
+++ lexer.kt
@@ -29,24 +29,27 @@ val specialCharacterTable = hashSetOf(
     '\\'
 )
 val ignoredCharacterTable = hashSetOf(' ', '\n', '\t')
-fun splitString(rawString:String): ArrayList<String> {
+fun splitString(rawString: String): ArrayList<String> {
     val splitStr = arrayListOf<String>()
-    var strBuffer=""
-    for(character in rawString){
-        if(specialCharacterTable.contains(character)){
-            if(strBuffer!=""){
+    var strBuffer = ""
+    for (character in rawString) {
+        if (character == '.' && strBuffer != "" && strBuffer.toIntOrNull()!=null) {
+            strBuffer+='.'
+        } else if (specialCharacterTable.contains(character)) {
+            if (strBuffer != "") {
                 splitStr.add(strBuffer)
-                strBuffer=""
+                strBuffer = ""
             }
             splitStr.add(character.toString())
-        }else if(ignoredCharacterTable.contains(character)){
-            if(strBuffer!=""){
+        } else if (ignoredCharacterTable.contains(character)) {
+            if (strBuffer != "") {
                 splitStr.add(strBuffer)
-                strBuffer=""
+                strBuffer = ""
             }
-        }else {
-            strBuffer+=character
+        } else {
+            strBuffer += character
         }
     }
     return splitStr
 }
+data class token(val type:Int,val value:String)
