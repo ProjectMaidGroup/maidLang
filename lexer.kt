e00f1bdb3c402e3c6f60ae8c6352448f4887c55d
_111y <huy264128@gmail.com>
2026-03-17 19:21:06 +0800

    rebuild lexer

--- lexer.kt
+++ lexer.kt
@@ -1,74 +1,95 @@
-val specialCharacterTable = hashSetOf(
-    '+',
-    '-',
+val operators = setOf(
+    '!',
+    '@',
+    '#',
+    '$',
+    '%',
+    '^',
+    '&',
     '*',
-    '/',
-    '=',
     '(',
     ')',
-    ',',
-    ';',
-    ':',
-    '&',
-    '^',
-    '%',
-    '$',
-    '#',
-    '@',
-    '!',
-    '<',
-    '>',
-    '?',
-    '\"',
-    '\'',
-    '.',
-    '[',
-    ']',
+    '-',
+    '+',
+    '=',
     '{',
     '}',
-    '\\'
+    ':',
+    ';',
+    '<',
+    '>',
+    ',',
+    '.',
+    '/',
+    '?',
+    '|'
 )
-val ignoredCharacterTable = hashSetOf(' ', '\n', '\t')
-fun splitString(rawString: String): ArrayList<String> {
-    val splitStr = arrayListOf<String>()
-    var strBuffer = ""
-    for (character in rawString) {
-        if (character == '.' && strBuffer != "" && strBuffer.toIntOrNull() != null) {
-            strBuffer += '.'
-        } else if (specialCharacterTable.contains(character)) {
-            if (strBuffer != "" && !specialCharacterTable.contains(strBuffer[0])) {
-                splitStr.add(strBuffer)
-                strBuffer = ""
-            }
-            if (strBuffer != "") {
-                if(character=='='&& setOf('+','-','*','/','^','&','|','%','=','!').contains(strBuffer[0])){
-                    strBuffer+=character
-                    splitStr.add(strBuffer)
-                    strBuffer=""
-                }else if (setOf('|','&','+','-').contains(character)&&strBuffer[0]==character){
-                    strBuffer+=character
-                    splitStr.add(strBuffer)
-                    strBuffer=""
-                }else{
-                    splitStr.add(strBuffer);
-                    splitStr.add(character.toString())
+
+data class token(val type: Int, val value: String) //define same as state
+
+suspend fun lexer(rawText: String) {
+    var state: Int = 0  // 0 -> Null Statement 1-> Identify 2->Int 3->Float 4:Char 5:String 6:Operator
+    var catchSlash: Boolean = false //  捕捉到了反斜杠时为 true
+    var lexerBuffer: String = ""
+    var tokenList = mutableListOf<token>()
+    for (character in rawText) {
+        when (state) {
+            0 -> {
+                when {
+                    character.isDigit() -> {
+                        lexerBuffer += character
+                        state = 2 //switch to Int mode
+                    }
+
+                    character == '.' -> {
+                        lexerBuffer = "."
+                        state = 6 //switch to Float mode
+                    }
+
+                    character == '\'' -> state = 4 //switch to Char mode
+
+                    character == '"' -> state = 5 //switch to String mode
+
+                    setOf(' ', '\n', '\t').contains(character) -> continue
+                    else -> state = 1 //switch to Identify mode
                 }
             }
 
-        } else if (ignoredCharacterTable.contains(character)) {
-            if (strBuffer != "") {
-                splitStr.add(strBuffer)
-                strBuffer = ""
+            1 -> {
+                if (operators.contains(character) || setOf('\\', '"', '\'', ' ', '\n', '\t').contains(character)) {
+                    tokenList.add(token(1, lexerBuffer))
+                    lexerBuffer = ""
+                    if (operators.contains(character)) {
+                        lexerBuffer += character
+                        state = 6
+                        continue
+                    }
+                    state = 0
+                } else lexerBuffer += character
             }
-        } else {
-            if (strBuffer != "" && specialCharacterTable.contains(strBuffer[0])) {
-                splitStr.add(strBuffer)
-                strBuffer = ""
+
+            2 -> {
+                if (character == '.' || character == 'e') {
+                    lexerBuffer += character
+                    state = 3
+                    continue
+                }
+                if (operators.contains(character) || setOf('\\', '"', '\'', ' ', '\n', '\t').contains(character)) {
+                    tokenList.add(token(1, lexerBuffer))
+                    lexerBuffer = ""
+                    if (operators.contains(character)) {
+                        lexerBuffer += character
+                        state = 6
+                        continue
+                    }
+                    state = 0
+                    continue
+                }
+                if (character.isDigit())
+                    lexerBuffer += character
+                else
+                    throw IllegalStateException("string $lexerBuffer is not a illegal value")
             }
-            strBuffer += character
         }
     }
-    return splitStr
 }
-
-data class token(val type: Int, val value: String?)
