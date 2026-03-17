8860aefa7260cdc578acf5134c7ab0d4a04808d2
_111y <huy264128@gmail.com>
2026-03-17 18:21:52 +0800

    making lexer

--- lexer.kt
+++ lexer.kt
@@ -33,23 +33,42 @@ fun splitString(rawString: String): ArrayList<String> {
     val splitStr = arrayListOf<String>()
     var strBuffer = ""
     for (character in rawString) {
-        if (character == '.' && strBuffer != "" && strBuffer.toIntOrNull()!=null) {
-            strBuffer+='.'
+        if (character == '.' && strBuffer != "" && strBuffer.toIntOrNull() != null) {
+            strBuffer += '.'
         } else if (specialCharacterTable.contains(character)) {
-            if (strBuffer != "") {
+            if (strBuffer != "" && !specialCharacterTable.contains(strBuffer[0])) {
                 splitStr.add(strBuffer)
                 strBuffer = ""
             }
-            splitStr.add(character.toString())
+            if (strBuffer != "") {
+                if(character=='='&& setOf('+','-','*','/','^','&','|','%','=','!').contains(strBuffer[0])){
+                    strBuffer+=character
+                    splitStr.add(strBuffer)
+                    strBuffer=""
+                }else if (setOf('|','&','+','-').contains(character)&&strBuffer[0]==character){
+                    strBuffer+=character
+                    splitStr.add(strBuffer)
+                    strBuffer=""
+                }else{
+                    splitStr.add(strBuffer);
+                    splitStr.add(character.toString())
+                }
+            }
+
         } else if (ignoredCharacterTable.contains(character)) {
             if (strBuffer != "") {
                 splitStr.add(strBuffer)
                 strBuffer = ""
             }
         } else {
+            if (strBuffer != "" && specialCharacterTable.contains(strBuffer[0])) {
+                splitStr.add(strBuffer)
+                strBuffer = ""
+            }
             strBuffer += character
         }
     }
     return splitStr
 }
-data class token(val type:Int,val value:String)
+
+data class token(val type: Int, val value: String?)
